60193f1ed29a0ae61b06693659aa041bc9a62163
ピクセル GM <35326620+PixelGM@users.noreply.github.com>
2023-04-05 14:35:40 -0700

    Ver. 2 Update! :3
    New Animations!
    
    Counter isn't showing if there's no text
    
    Button change to "Coppied!" after copying
    
    Auto Updates, no need button!

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function updateText() {
 	var chunks = [];
 	var currentChunk = "";
 	var words = input.split(" ");
-	for(var i = 0; i < words.length; i++) {
-		if((currentChunk + words[i] + " ").length <= 1980) {
+	for (var i = 0; i < words.length; i++) {
+		if ((currentChunk + words[i] + " ").length <= 1980) {
 			currentChunk += words[i] + " ";
 		} else {
 			currentChunk += "WILL CONTINUE.";
@@ -12,19 +12,35 @@ function updateText() {
 			currentChunk = "CONTINUE: " + words[i] + " ";
 		}
 	}
-	if(currentChunk !== "") {
+	if (currentChunk !== "") {
 		chunks.push(currentChunk.trim());
 	}
 	var output = "";
-	for(var i = 0; i < chunks.length; i++) {
-		output += '<div class="text-container"><textarea class="text" readonly>' + chunks[i].replace(/</g, "&lt;").replace(/>/g, "&gt;") + '</textarea><button class="copy-button" onclick="copyText(event)">Copy</button></div>';
+	for (var i = 0; i < chunks.length; i++) {
+		output += '<div class="text-container"><textarea class="text" readonly>' + chunks[i].replace(/</g, "&lt;").replace(/>/g, "&gt;") + '</textarea>';
+		if (input !== '') {
+			output += '<button class="copy-button" onclick="copyText(event)">Copy</button>';
+		}
+		output += '</div>';
 	}
 	document.getElementById("output").innerHTML = output.trim();
 }
+document.getElementById("input").addEventListener("input", updateText);
 
 function updateCounter() {
 	var inputLength = document.getElementById("input").value.length;
-	if(inputLength > 0) {
+	if (inputLength > 0) {
+		var prompts = Math.ceil(inputLength / 2000);
+		document.getElementById("counter").innerHTML = inputLength + "/2000 = " + prompts + " prompts";
+	} else {
+		document.getElementById("counter").innerHTML = "";
+	}
+}
+document.getElementById("input").addEventListener("input", updateText);
+
+function updateCounter() {
+	var inputLength = document.getElementById("input").value.length;
+	if (inputLength > 0) {
 		var prompts = Math.ceil(inputLength / 2000);
 		document.getElementById("counter").innerHTML = inputLength + "/2000 = " + prompts + " prompts";
 	} else {
@@ -34,5 +50,15 @@ function updateCounter() {
 
 function copyText(event) {
 	var text = event.target.previousSibling.value;
-	navigator.clipboard.writeText(text);
+	if (text === '') {
+		event.target.remove();
+	} else {
+		navigator.clipboard.writeText(text).then(function() {
+			/* success */
+			event.target.innerHTML = 'Copied!';
+		}, function() {
+			/* failure */
+			alert('Failed to copy text');
+		});
+	}
 }
